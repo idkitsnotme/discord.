1227508337d0e653c3f5d9c961ee30031c3d995c
idkitsnotme <bazick8@gmail.com>
2026-02-20 16:05:55 -0800

    Refactor 'someone' command to include message parameter

--- main.py
+++ main.py
@@ -3,17 +3,14 @@ import os
 import random
 from discord.ext import commands
 
-# Railway will look for this name in the "Variables" tab
 TOKEN = os.getenv("DISCORD_TOKEN") 
 
 intents = discord.Intents.default()
-intents.members = True          # Needs to be ON in Dev Portal
-intents.message_content = True   # Needs to be ON in Dev Portal
+intents.members = True          
+intents.message_content = True   
 
-# Prefix is '@', so you type '@someone'
 bot = commands.Bot(command_prefix='@', intents=intents)
 
-# This is the "Guest List" we talked about
 server_cache = {}
 
 @bot.event
@@ -21,21 +18,26 @@ async def on_ready():
     print(f'Logged in as {bot.user}')
 
 @bot.command()
-async def someone(ctx):
+async def someone(ctx, *, message: str = ""):
     server_id = ctx.guild.id
 
-    # If we don't have a list for this server yet, make one
+    # Build cache if it doesn't exist
     if server_id not in server_cache:
-        # Puts everyone who isn't a bot into the list
         server_cache[server_id] = [m.mention for m in ctx.guild.members if not m.bot]
 
     if server_cache[server_id]:
         rando = random.choice(server_cache[server_id])
-        await ctx.send(f"I choose you: {rando}")
+        
+        # If the user added text (like "farted"), it adds it after the mention
+        if message:
+            await ctx.send(f"{rando} {message}")
+        else:
+            # Default response if you just type @someone with no text
+            await ctx.send(f"I choose you: {rando}")
     else:
-        await ctx.send("The server seems empty... or I don't have permission to see members!")
+        await ctx.send("I couldn't find anyone in the guest list!")
 
-# This keeps the guest list updated so you don't ping people who left
+# Cache update events (Keep these the same as before)
 @bot.event
 async def on_member_join(member):
     if member.guild.id in server_cache and not member.bot:
