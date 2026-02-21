99ac2d0f1b007b0d1302863963b1bebab585c420
idkitsnotme <bazick8@gmail.com>
2026-02-20 16:12:28 -0800

    Enhance user selection logic in 'someone' command
    Refactor 'someone' command to avoid repeating last picked user and improve list management.

--- main.py
+++ main.py
@@ -12,30 +12,36 @@ intents.message_content = True
 bot = commands.Bot(command_prefix='@', intents=intents)
 
 server_cache = {}
+last_picked = {} # Tracks the last person picked per server
 
 @bot.event
-async def __on_ready__():
+async def on_ready():
     print(f'Logged in as {bot.user}')
 
 @bot.command()
 async def someone(ctx, *, user_input: str = ""):
     server_id = ctx.guild.id
 
-    # 1. Build the list of humans only
+    # 1. Build/Refresh the list
     if server_id not in server_cache or not server_cache[server_id]:
         server_cache[server_id] = [m.mention for m in ctx.guild.members if not m.bot]
 
-    # 2. Pick EXACTLY one person
-    if server_cache[server_id]:
-        rando = random.choice(server_cache[server_id])
+    # 2. Filter out the last person picked so we don't repeat
+    current_pool = server_cache[server_id].copy()
+    if server_id in last_picked and len(current_pool) > 1:
+        if last_picked[server_id] in current_pool:
+            current_pool.remove(last_picked[server_id])
+
+    # 3. Pick from the new pool
+    if current_pool:
+        rando = random.choice(current_pool)
+        last_picked[server_id] = rando # Remember this person for next time
         
-        # 3. Clean the user_input to make sure no extra pings snuck in
-        # We only send the one 'rando' we picked
         await ctx.send(f"{rando} {user_input}")
     else:
-        await ctx.send("I can't find anyone to ping!")
+        await ctx.send("The list is empty!")
 
-# Keep the list updated
+# Cache update events (Keep these)
 @bot.event
 async def on_member_join(member):
     if member.guild.id in server_cache and not member.bot:
